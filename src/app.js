b04ecdaa1a9cad8cb322803c1da1d3d8fea20f68
zeeshan <144zeeshansiddiqui@gmail.com>
2021-10-26 13:21:13 +0530

    first trial version

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path') 
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { defaultCipherList } = require('constants')
+
+const app = express()
+
+// Define path for express config
+const publicDirPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials') 
+
+//setup handlebars engine and location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+  
+app.get('', (req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Zeeshan Siddiqui'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Page',    
+        name:'Zeeshan Siddiqui'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help page',
+        name:'Zeeshan Siddiqui'
+    })
+})
+
+
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404 page',
+        name:'Zeeshan Siddiqui',
+        errorMessage:'help article not found'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        res.send({
+            error:'No Address Provided'
+        })
+    } 
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error){
+            res.send({error})
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+           if(error) {
+               res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location
+            })
+        })
+    })
+   
+})     
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404 page',
+        name:'Zeeshan Sidiqui',
+        errorMessage:'Page not found'
+    })
+
+})
+
+// 
+
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, ()=>{
+    console.log('setting up server on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = ( latitude,longitude,callback)=>{
+    const url = 'http://api.weatherstack.com/current?access_key=27ec775486ae958a06927f70c12a3cdc&query='+latitude + ',' +longitude+'&unit=m'
+
+    request({url, json:true },(error, response)=>{
+        if(error){
+            callback('unable to connect to weather service' , undefined)
+        } else if(response.body.error){
+            callback('location not found', undefined)
+        } else{
+            callback(undefined, 'it is currently '+ response.body.current.temperature + ' degree out . It feels like '+ response.body.current.feelslike +' degree out' + ' with visiblity '+response.body.current.visibility )
+        }
+
+    })
+}
+
+
+module.exports = forecast
