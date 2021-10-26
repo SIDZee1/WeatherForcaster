38a8ca221f892230f1b8efe1ebc5f723f5582eab
zeeshan <144zeeshansiddiqui@gmail.com>
2021-10-26 13:23:20 +0530

    v2 console statement removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log ('client site java script file is loaded!!')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
